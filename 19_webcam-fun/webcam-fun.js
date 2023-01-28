3911983f91e9ff9b353357ec3cac338bdb1be335
Tsonga Dog <Alimbetov_S0930@kt.nis.edu.kz>
2023-01-28 22:21:11 +0600

    Finished "Unreal Webcam Fun" project

--- 19_webcam-fun/webcam-fun.js
+++ 19_webcam-fun/webcam-fun.js
@@ -26,8 +26,8 @@ function paintToCanvas(){
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
-        ctx.globalAlpha = 0.5;
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.5;
         // pixels = greenScreen(pixels);
         ctx.putImageData(pixels, 0, 0);
     }, 16);
@@ -74,11 +74,11 @@ function greenScreen(pixels){
         const red = pixels.data[i];
         const green = pixels.data[i + 1];
         const blue = pixels.data[i + 2];
-        let alpha = pixels.data[i + 3];
+        const alpha = pixels.data[i + 3];
 
         if( red >= levels.rmin && green >= levels.gmin && blue >= levels.bmin &&
             red <= levels.rmax && green <= levels.gmax && blue <= levels.bmax){
-            alpha = 0;
+            pixels.data[i + 3]= 0;
         }
     }
     return pixels;
