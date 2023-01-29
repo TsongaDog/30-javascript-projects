1f266a70e6273847fbf42284bc366292e3fb5b36
Tsonga Dog <Alimbetov_S0930@kt.nis.edu.kz>
2023-01-29 20:47:43 +0600

    Finished "Sticky Nav" project

---
+++ 24_sticky-nav/sticky-nav.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('#main');
+const logo = document.querySelector('.logo');
+let navTop = nav.offsetTop
+
+function fixNav() {
+    if (window.scrollY >= navTop) {
+        nav.classList.add('fixed-nav');
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+    } else {
+        nav.classList.remove('fixed-nav');
+        document.body.style.paddingTop = 0;
+    }
+}
+
+window.addEventListener('scroll', fixNav);
